c663b1606d98c87b4cca731f43bf51afd1086fbe
jaeho.lee <jaeho.lee@barunn.net>
2023-07-07 15:12:59 +0900

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+mv mysite _mysite
+git clone https://github.com/iameeo/pythonanywhere_iameeo.git
+mv /home/iameeo/pythonanywhere_iameeo/mysite/ /home/iameeo/
+rm -rf pythonanywhere_iameeo/
+rm -r _mysite/
